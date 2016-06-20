f72c0d82f26b599ddff9bd24e4306bdcdf8ff644
aaron <aarongitis@gmail.com>
2016-06-20 10:06:45 -0700

    Add fragment logic for 1 player; simplified version

--- Spindown/Spindown/app/src/main/java/badassapps/aaron/spindown/MainActivity.java
+++ Spindown/Spindown/app/src/main/java/badassapps/aaron/spindown/MainActivity.java
@@ -1,15 +1,25 @@
 package badassapps.aaron.spindown;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getFragmentManager();
+
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragmentHolder);
+
+        if (fragment == null) {
+            fragment = new LifeCounterFragment();
+            fragmentManager.beginTransaction().add(R.id.fragmentHolder, fragment).commit();
+        }
     }
 }
